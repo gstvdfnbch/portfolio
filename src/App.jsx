50d81b505408965688e67b7c80c18b99fb909082
gstv <gustavodiefenbach@gmail.com>
2025-03-04 13:23:04 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Menu from './pages/Menu'
 
 function App() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -7,15 +8,7 @@ function App() {
 
   return (
     <>
-      <div
-        className={`menu ${isExpanded ? "expanded" : ""} ${isExiting ? "exiting" : ""}`}
-        onMouseEnter={() => setIsExpanded(true)}  // Define como true ao passar o mouse
-        onMouseLeave={() => setIsExpanded(false)} // Define como false ao sair
-      >
-        <span className={`left ${isExpanded ? "show" : ""}`}>DEV</span>
-        <span>GSTV</span>
-        <span className={`right ${isExpanded ? "show" : ""}`}>ART</span>
-      </div>
+      <Menu />
     </>
   );
 }
